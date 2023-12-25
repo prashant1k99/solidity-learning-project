ac92d968fda1cd9172cefb8c55b77fee798aec7a
prashant1k99 <prashantco111@gmail.com>
2023-12-25 16:41:03 +0530

    Modifier explanation for solidity Sample Implementation

--- contracts/SolBasic.sol
+++ contracts/SolBasic.sol
@@ -206,3 +206,43 @@ contract Product {
         owner = newOwner;
     }
 }
+
+// Another Exercise:
+contract PausableToken {
+    address public owner;
+    bool public paused;
+    mapping(address => uint) public balances;
+
+    constructor() {
+        owner = msg.sender;
+        paused = false;
+        balances[owner] = 1000;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You do not have permission to access this.");
+        _;
+    }
+
+    // 2️⃣ Implement the modifier to check if the contract is not paused
+    modifier notPaused() {
+        require(paused == false, "The Contract is Paused");
+        _;
+    }
+
+    function pause() public onlyOwner {
+        paused = true;
+    }
+
+    function unpause() public onlyOwner {
+        paused = false;
+    }
+
+    // 3️⃣ use the notPaused modifier in this function 
+    function transfer(address to, uint amount) public notPaused {
+        require(balances[msg.sender] >= amount, "Insufficient balance");
+
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+    }
+}
